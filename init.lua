3887ca6efe44cf413e151c6d796575332bfb51d6
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-02-23 11:03:00 +0530

    setup packer and install script

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 --should contain default configs and packer installers
 vim.opt.number = true
+
+require('packer').startup({function()
+  -- plugins here
+end,
+config = {
+  display = {
+    open_fn = require('packer.util').float,
+  }
+}})
