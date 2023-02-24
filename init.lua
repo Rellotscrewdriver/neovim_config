395b36758f70ae120130c21b8a03f7adf7271ed3
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-02-24 09:26:09 +0530

    setup sourcing multiple files

--- init.lua
+++ init.lua
@@ -1,11 +1,34 @@
---should contain default configs and packer installers
-vim.opt.number = true
+--This file is the starting entry of My configuration
 
+--tables for sourcing configs
+local confPlug_files = {
+
+}
+
+local confcore_files = {
+  "settings.lua",
+}
+
+--function to source tables
+local source_files = function(conf_table, conf_path)
+  for _, name in ipairs(conf_table) do
+    local path = string.format(conf_path, vim.fn.stdpath("config"), name)
+    local source_cmd = "source " .. path
+    vim.cmd(source_cmd)
+  end
+end
+
+--"""Setup Packer"""
 require('packer').startup({function()
   -- plugins here
+  use 'wbthomason/packer.nvim'
 end,
 config = {
   display = {
     open_fn = require('packer.util').float,
   }
 }})
+
+--"""source configurations"""
+source_files(confPlug_files, "%s/configPlug/%s")
+source_files(confcore_files, "%s/core/%s")
