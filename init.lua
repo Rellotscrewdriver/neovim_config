2b1b65f8a15fe01be052365cffb2c39ee660188e
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-07 09:19:31 +0530

    Setup impatient to improve startuptime

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+_G.__luacache_config = {
+  chunks = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_chunks',
+  },
+  modpaths = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_modpaths',
+  }
+}
+require('impatient')
 --starting entry
 local confPlug_files = {
   "completion.lua",
