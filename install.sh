e35f912ce0191bb3bf36aee90cf08df6f652c378
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-08 19:48:09 +0530

    added a script to automatically install this config

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@ NVIM_DIR=$HOME/tools/nvim
 NVIM_CONFIG_DIR=$HOME/.config/nvim
 
 echo "Installing Packer"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+echo "Copying config files to ~/.config/nvim"#
+cp -r configPlug core snippets init.lua $NVIM_CONFIG_DIR  
 
 echo "Installing nvim plugins, please wait"
 "$NVIM_DIR/bin/nvim" -c "autocmd User PackerComplete quitall" -c "PackerSync"
 
 echo "Installing and Compiling Tree-Sitter prasers"
-"$NVIM_DIR/bin/nvim" nvim -es "TSinstall cpp cmake c make markdown"
-echo "Finished installing Nvim and its dependencies!"
+"$NVIM_DIR/bin/nvim" nvim -es "TSinstall cpp cmake make markdown"
+
+echo "Finished installing the config and its plugin dependencies!"
+
