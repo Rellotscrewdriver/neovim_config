44e54cbd01308db2e7ecc652c912ec042ae8eec8
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-14 17:23:20 +0530

    Added offset side for file explorer

--- configPlug/window.lua
+++ configPlug/window.lua
@@ -1,30 +1,56 @@
-require("bufferline").setup{
-	options = {
-		diagnostics = "nvim_lsp",
-		indicator = {
-    	            --icon = '▎', -- this should be omitted if indicator style is not 'icon'
-    	            style = 'icon',
-    	},
-    	buffer_close_icon = '',
-  groups = {
-    options = {
-      toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+require('bufferline').setup {
+  options = {
+    mode = "buffers", -- set to "tabs" to only show tabpages instead
+    numbers = "none",
+    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+    indicator = {
+        icon = '▎', -- this should be omitted if indicator style is not 'icon'
+        style = 'icon',
     },
-    items = {
-      {
-        name = "Docs",
-        highlight = {undercurl = true, sp = "green"},
-        auto_close = false,  -- whether or not close this group if it doesn't contain the current buffer
-        matcher = function(buf)
-          return buf.filename:match('%.md') or buf.filename:match('%.txt')
-        end,
-        separator = { -- Optional
-          style = require('bufferline.groups').separator.tab
-        },
-      }
-    }
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    --- name_formatter can be used to change the buffer's label in the bufferline.
+    --- Please note some names can/will break the
+    --- bufferline so use this at your discretion knowing that it has
+    --- some limitations that will *NOT* be fixed.
+    --name_formatter = function(buf)  -- buf contains:
+          -- name                | str        | the basename of the active file
+          -- path                | str        | the full path of the active file
+          -- bufnr (buffer only) | int        | the number of the active buffer
+          -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
+          -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
+    --end,
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    truncate_names = true, -- whether or not tab names should be truncated
+    tab_size = 18,
+    diagnostics = "none",
+    diagnostics_update_in_insert = false,
+    offsets = {
+            {
+              text = 'File Explorer',
+              filetype = 'neo-tree',
+              highlight = 'PanelHeading',
+              text_align = 'center',
+              separator = true,
+            },
+    },
+    color_icons = true, -- whether or not to add the filetype icon highlights
+    show_buffer_icons = true, -- disable filetype icons for buffers
+    show_buffer_close_icons = true,
+    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+    show_close_icon = true,
+    show_tab_indicators = true,
+    show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    separator_style = "slant",-- | "slope" | "thick" | "thin" | { 'any', 'any' },
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
   }
-
-	}
-
 }
