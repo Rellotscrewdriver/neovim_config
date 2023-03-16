558e6d8550776ab9711e205fc08ddea7d32f1f59
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-16 09:08:11 +0530

    removed double comment in autocommands.lua

--- core/autocommands.lua
+++ core/autocommands.lua
@@ -1,7 +1,8 @@
 -- Disable folding on alpha buffer
 vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
-]])--open Neotree on startup
+]])
+
 --open Neotree on startup
 vim.cmd[[
     augroup NEOTREE_AUGROUP
