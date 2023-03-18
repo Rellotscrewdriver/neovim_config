9d22fb485ffbc2603fbe784f771d2072cf34e3e3
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-18 11:15:26 +0530

    Hightlight close button when in foucs

--- configPlug/window.lua
+++ configPlug/window.lua
@@ -8,7 +8,7 @@ require('bufferline').setup {
       tab_close = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
       close_button = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
       close_button_visible = { fg = c.surface1, bg = c.mantle, bold = false, italic = false },
-      close_button_selected = { fg = c.surface1, bg = c.base, bold = false, italic = false },
+      close_button_selected = { fg = c.text, bg = c.base, bold = false, italic = false },
       },
     },
   },
