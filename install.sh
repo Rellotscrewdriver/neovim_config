60b96cd67b36e1b7abb34d8c69281610f32d77a0
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-03-30 10:08:36 +0530

    fixed the errors and wrong directories when running install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+NVIM_DIR=/usr/bin/nvim
+NVIM_CONFIG_DIR=~/.config/nvim
 NVIM_DIR=$HOME/tools/nvim
 NVIM_CONFIG_DIR=$HOME/.config/nvim
 
@@ -7,11 +9,20 @@ git clone https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pa
 echo "Copying config files to ~/.config/nvim"#
 cp -r configPlug core snippets init.lua $NVIM_CONFIG_DIR  
 
-echo "Installing nvim plugins, please wait"
-"$NVIM_DIR/bin/nvim" -c "autocmd User PackerComplete quitall" -c "PackerSync"
 
-echo "Installing and Compiling Tree-Sitter prasers"
-"$NVIM_DIR/bin/nvim" nvim -es "TSinstall cpp cmake make markdown"
+echo -n "Copying config files to ~/.config/nvim"
+mkdir -p ~/.config/nvim
+cp -r lua snippets init.lua $NVIM_CONFIG_DIR 
+echo "...done"
+
+echo -n "Installing nvim plugins, please wait"
+"$NVIM_DIR" nvim -es "Lazy install"
+echo "...done"
+
+echo -n "Installing and Compiling Tree-Sitter prasers"
+"$NVIM_DIR" nvim -es "TSinstall cpp cmake make markdown markdown-inline python"
+echo "...done"
 
 echo "Finished installing the config and its plugin dependencies!"
+echo "Enjoy!"
 
