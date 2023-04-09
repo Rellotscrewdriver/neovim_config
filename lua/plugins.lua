eb69e716908d3125c95c07df883ac34302cc494d
Rellot's Screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2023-04-09 12:18:33 +0530

    Removed lazy loading in some plugins and formatted some lines

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,19 +3,19 @@ return {
   {'hrsh7th/nvim-cmp', lazy = true},
   {'neovim/nvim-lspconfig', lazy = true},
   {'hrsh7th/cmp-nvim-lsp', lazy = true},
-  {'hrsh7th/cmp-buffer', lazy = true},
-  {'hrsh7th/cmp-path', lazy = true},
+  {'hrsh7th/cmp-buffer', lazy = false},
+  {'hrsh7th/cmp-path', lazy = false},
   {'hrsh7th/cmp-cmdline', lazy = false},
-  {'saadparwaiz1/cmp_luasnip', lazy = true},
+  {'saadparwaiz1/cmp_luasnip', lazy = false},
   {'L3MON4D3/LuaSnip', lazy = true}, --for snippets
   -- for additional features like outline, hovering docs and what not
-	{"glepnir/lspsaga.nvim", event = "LspAttach", dependencies = {{"nvim-tree/nvim-web-devicons"}, {"nvim-treesitter/nvim-treesitter"}}}, 
+  {"glepnir/lspsaga.nvim", event = "LspAttach", dependencies = {{"nvim-tree/nvim-web-devicons"}, {"nvim-treesitter/nvim-treesitter"}}}, 
   {"folke/trouble.nvim", requires = "nvim-tree/nvim-web-devicons"}, --for summarising elements in the code
   {"williamboman/mason.nvim"}, --portable server manager 
   {"nvim-lua/plenary.nvim", lazy = true},
-	--for showing windows in neovim
+  --for showing windows in neovim
   {'akinsho/bufferline.nvim', commit = "b337fd393cef2e3679689d220e2628722c20ddcb", dependencies = 'nvim-tree/nvim-web-devicons', lazy = true},
-	--Fuzzy Finding
+  --Fuzzy Finding
   {'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = 'nvim-lua/plenary.nvim', lazy = true},
   {'CRAG666/code_runner.nvim', dependencies = 'nvim-lua/plenary.nvim', lazy = true}, -- for running a single file quickly
   'goolord/alpha-nvim', --Showing dashboard
@@ -23,10 +23,10 @@ return {
   {'Civitasv/cmake-tools.nvim', lazy = true}, --Manage a cmake project
   {'lewis6991/gitsigns.nvim', lazy = true }, --OOBE git
   'rcarriga/nvim-notify', --Show notifications
-	--File explorer
+  --File explorer
   {"nvim-neo-tree/neo-tree.nvim", branch = "v2.x", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",}},
-	--for showing up neovim on Discord Rich Presence
-	{'andweeb/presence.nvim', lazy = true},
+  --for showing up neovim on Discord Rich Presence
+  {'andweeb/presence.nvim', lazy = true},
   {'nvim-lualine/lualine.nvim', lazy = true}, --statusline
   {'windwp/nvim-autopairs', lazy = true}, --quickly put curly braces, brackets on both sides
   {"Shatur/neovim-session-manager", lazy = true}, --for managing sessions
@@ -39,10 +39,7 @@ return {
   {'famiu/bufdelete.nvim', lazy = true}, --for deleting buffers effectively
   {"nvim-telescope/telescope-project.nvim", lazy = true}, --for managing projects easily
   {"p00f/nvim-ts-rainbow", lazy = true}, --uhh, for cool braces?
-	--for cool looking braces
-  {"nvim-treesitter/nvim-treesitter", lazy = true},
+  {"nvim-treesitter/nvim-treesitter", lazy = true}, --for syntax highlighting
 	--for debugging
   {"rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"}, lazy = true},
 }
-
-
