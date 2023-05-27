4643a19aae1922a2fc4693e3dcd2fb423bb9fe19
Rellot's Screwdriver <JKanimations71@gmail.com>
2023-05-27 20:09:01 +0530

    Removed autocommand of automatically opening neotree on startup

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -4,8 +4,3 @@ vim.cmd([[
 ]])
 
 --open Neotree on startup
-vim.cmd[[
-    augroup NEOTREE_AUGROUP
-      autocmd!
-      au VimEnter * lua vim.defer_fn(function() vim.cmd("Neotree show left") end, 10)
-    augroup END ]]      
